aba5b61cd3124e5294a9799f0921f04d2469bd99
robertnjunge <mubearobert@gmail.com>
2023-10-18 19:44:47 +0300

    completed assignment

--- lib/get.py
+++ lib/get.py
@@ -5,6 +5,8 @@ url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locatio
 
 response = requests.get(url)
 
+print(response.text)
+
 json_content = json.loads(response.text)
 
 print(json.dumps(json_content, indent=4))
